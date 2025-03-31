084c196679ffdacfe409d844e01fb13bda82bd81
angus-mcritchie <53469513+angus-mcritchie@users.noreply.github.com>
2025-03-31 09:33:10 +0000

    Fix styling

--- example-output.php
+++ example-output.php
@@ -2,13 +2,13 @@
 
 $__boostDirectiveArguments = [];
 
-if (!($__boostDirectiveKey = $__boostDirectiveArguments[0] ?? null)) {
+if (! ($__boostDirectiveKey = $__boostDirectiveArguments[0] ?? null)) {
     throw new \InvalidArgumentException('The name of the cache key is required.');
 }
 
 $__boostDirectiveReplacements = $__boostDirectiveArguments[1] ?? null;
 $__boostDirectiveStore = $__boostDirectiveArguments[2] ?? 'array';
-$__boostDirectiveCallback = (fn(array $__boostDirectiveArguments) => function () use ($__boostDirectiveArguments) {
+$__boostDirectiveCallback = (fn (array $__boostDirectiveArguments) => function () use ($__boostDirectiveArguments) {
     extract($__boostDirectiveArguments, EXTR_SKIP);
     ob_start();
 
