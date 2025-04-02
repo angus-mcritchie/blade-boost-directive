746ca64346ee90f65a50567e0f8ef5d0a56aa232
Angus McRitchie <angus@hobbiesdirect.com.au>
2025-04-02 14:10:28 +1100

    added comments

--- src/BoostServiceProvider.php
+++ src/BoostServiceProvider.php
@@ -6,14 +6,28 @@ use Illuminate\Support\Facades\Blade;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
+/**
+ * Service provider for the Blade Boost Directive package.
+ *
+ * This provider registers the @boost and @endboost Blade directives
+ * and handles the package configuration.
+ */
 class BoostServiceProvider extends PackageServiceProvider
 {
+    /**
+     * Configure the package.
+     */
     public function configurePackage(Package $package): void
     {
         $package->name('blade-boost-directive')
             ->hasConfigFile('blade-boost-directive');
     }
 
+    /**
+     * Boot the package services.
+     *
+     * @return void
+     */
     public function packageBooted()
     {
         if (! config()->get('blade-boost-directive.enabled')) {
